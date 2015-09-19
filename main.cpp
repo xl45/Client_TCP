7f748dc488b6a6cc6652e090c57a67b6c6e47a77
xl45 <rickielau1@hotmail.com>
2015-09-19 00:13:52 -0400

    add command line parameter

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include "WebClient.h"
 
-int main(){
+int main(int argc, char* argv[]){
     // take command line arguments
-    WebClient myWebClient("127.0.0.1", "3480", "p", "filelist.txt");
+    if( argc !=  5 ){
+        std::cout << "oops, wrong parameter format, refer README.txt for detail.\n";
+        return 1;
+    }
+    std::string server_host = argv[1];
+    std::string server_port = argv[2];
+    std::string conn_type = argv[3];
+    std::string filelist = argv[4];
+
+    // initialize a web client and start to work
+    WebClient myWebClient(server_host, server_port, conn_type, filelist);
     myWebClient.client_send_recv();
 }
